fe23da2772982eff0d4cd759e633c961b222e218
punchyface <s10222101@connect.np.edu.sg>
2022-01-15 01:00:51 +0800

    add pop-up to view details & add loading bar

--- js/index.js
+++ js/index.js
@@ -28,19 +28,58 @@ $(document).ready(function () {
                     
                     <h2>${response[i].foodname}</h2>
                     <p>$${response[i].foodprice.toFixed(2)}</p>
-                    <p><a class="btn btn-secondary" href="#" data-name='${response[i].foodname}' data-email='${response[i].foodprice}' data_id='${response[i]._id}' >View details &raquo;</a></p>
+                    <p>
+                      <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModalCenter" data-foodname='${response[i].foodname}' data-foodprice='${response[i].foodprice}' data-foodid='${response[i]._id}' data-fooddescription='${response[i].fooddescription}' data-foodimageurl='${response[i].foodimageurl}' >
+                        View details &raquo;
+                      </button>
+                    </p>
                 </div>`
                 
             }
 
             document.querySelector("body > main > div.container.marketing > div").innerHTML = content;
         }); 
-
-
-        document.querySelector("col-lg-4").on("click", "a", function () {
-            let idtobedeleted = $(this).data("id");
-            deleteForm(idtobedeleted);
-        })
     }
+    //to close pop up
+    $(".modal").on("click", "button.close" , function(){
+        $('.modal').modal('hide');
+    })
+    $(".modal").on("click", "button.btn.btn-secondary" , function(){
+        $('.modal').modal('hide');
+    })
+
+    //for popup when click view details
+    $("div.row").on("click", ".btn-secondary", function () {
+        let foodname = $(this).data("foodname");
+        console.log($(this).data());
+        let foodprice = $(this).data("foodprice");
+        let foodid = $(this).data("_id");
+        let foodimageurl = $(this).data("foodimageurl");
+        let fooddescription = $(this).data("fooddescription");
+        let content = `
+        <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalreLongTitle">${foodname}</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+            </div>
+            <div class="modal-body">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-md-6"><img src="${foodimageurl}" alt="${foodname}" width="200" height="200"></div>
+                        <div class="col-md-2 ml-auto"><p class="foodprice">$${foodprice.toFixed(2)}</p></div>
+                    </div>
+                </div>
+                <p>${fooddescription}</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Add to Cart</button>
+            </div>
+        </div>`;
+        document.querySelector("div.modal-content").innerHTML = content;
+        $('.modal').modal('show');
+
+    })
 })
 
