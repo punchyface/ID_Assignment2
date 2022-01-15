29663f44d2eebb35497b4326ca601312ebfb772e
punchyface <s10222101@connect.np.edu.sg>
2022-01-15 21:27:54 +0800

    categorise food and add scroll to category

--- js/index.js
+++ js/index.js
@@ -1,8 +1,43 @@
 $(document).ready(function () {
     const APIKEY = '61e0110da0f7d226f9b75dbc';
-    getMenu();
+    getCategory();
 
-    function getMenu(limit = 10, all = true){
+
+
+    //create foodcat div.row
+    function getCategory(){
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://onlinefood-ef2c.restdb.io/rest/foodcategory",
+            "method": "GET",
+            "headers": {
+              "content-type": "application/json",
+              "x-apikey": APIKEY,
+              "cache-control": "no-cache"
+            }
+        }
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            let content ="";
+            for (var i = 0; i < response.length; i++) {
+                
+                content = `${content}<div class="row" id="${response[i].foodcat}"><h3>${response[i].foodcatname}</h3></div>`
+            }
+            document.querySelector("div.container.marketing").innerHTML = content;
+        });
+        
+        getMenu();
+    }
+
+    function catarea(foodcat, content){
+        console.log(foodcat)
+        document.querySelector(`body > main > div.container.marketing > div#${foodcat}`).innerHTML += content;
+    }
+
+
+    function getMenu(){
         var settings = {
             "async": true,
             "crossDomain": true,
@@ -18,10 +53,11 @@ $(document).ready(function () {
         $.ajax(settings).done(function (response) {
             console.log(response);
             
-            let content = "";
+            
 
-            for (var i = 0; i < response.length && i < limit; i++) {
-                content = `${content}
+            for (var i = 0; i < response.length; i++) {
+                let content = "";
+                content = `
                 <div class="col-lg-4">
                     <title>${response[i].foodname}</title>
                     <img class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: 140x140" preserveAspectRatio="xMidYMid slice" focusable="false" src="${response[i].foodimageurl}"/>
@@ -29,17 +65,19 @@ $(document).ready(function () {
                     <h2>${response[i].foodname}</h2>
                     <p>$${response[i].foodprice.toFixed(2)}</p>
                     <p>
-                      <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModalCenter" data-foodname='${response[i].foodname}' data-foodprice='${response[i].foodprice}' data-foodid='${response[i]._id}' data-fooddescription='${response[i].fooddescription}' data-foodimageurl='${response[i].foodimageurl}' >
+                    <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModalCenter" data-foodname='${response[i].foodname}' data-foodprice='${response[i].foodprice}' data-foodid='${response[i]._id}' data-fooddescription='${response[i].fooddescription}' data-foodimageurl='${response[i].foodimageurl}' >
                         View details &raquo;
-                      </button>
+                    </button>
                     </p>
-                </div>`
-                
-            }
+                </div>`;
 
-            document.querySelector("body > main > div.container.marketing > div").innerHTML = content;
+                catarea(response[i].foodcat[0].foodcat, content);  
+            }
+            removeemptycategory()
         }); 
     }
+
+
     //to close pop up
     $(".modal").on("click", "button.close" , function(){
         $('.modal').modal('hide');
@@ -49,7 +87,7 @@ $(document).ready(function () {
     })
 
     //for popup when click view details
-    $("div.row").on("click", ".btn-secondary", function () {
+    $("div.marketing").on("click", ".btn-secondary", function () {
         let foodname = $(this).data("foodname");
         console.log($(this).data());
         let foodprice = $(this).data("foodprice");
@@ -79,7 +117,53 @@ $(document).ready(function () {
         </div>`;
         document.querySelector("div.modal-content").innerHTML = content;
         $('.modal').modal('show');
-
     })
+
+    function removeemptycategory(){
+        let rowlist = document.querySelectorAll("div.row");
+        for (i = 0; i < rowlist.length; i++){
+            
+            if (rowlist[i].querySelector('.col-lg-4') == null) {
+                rowlist[i].remove();
+            } 
+    
+        }
+        navcategory();
+
+    }
+
+    function navcategory(){
+        let rowlist = document.querySelectorAll("div.row");
+        for (i = 0; i < rowlist.length; i++){
+            let title = rowlist[i].querySelector('h3').textContent;
+            content = `<li><a class="dropdown-item" href="#${rowlist[i].id}">${title}</a></li>`;
+            document.querySelector(".dropdown-menu").innerHTML += content;
+        } 
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
 
