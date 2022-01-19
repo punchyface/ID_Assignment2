be30ad02907776a0ea1a18f261e7c8e69494d32f
punchyface <s10222101@connect.np.edu.sg>
2022-01-19 16:16:38 +0800

    update to new web design

---
+++ a-miscellaneous/oldweb/js/seller.js
@@ -0,0 +1,116 @@
+$(document).ready(function () {
+    const APIKEY = '61e0110da0f7d226f9b75dbc';
+    getCategory()
+
+    function getCategory(){
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://onlinefood-ef2c.restdb.io/rest/foodcategory",
+            "method": "GET",
+            "headers": {
+                "content-type": "application/json",
+                "x-apikey": APIKEY,
+                "cache-control": "no-cache"
+            }
+        }
+          
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            //for the cards
+            let content ="";
+            for (var i = 0; i < response.length; i++) {
+                
+                content = `${content}<div class="row" id="${response[i].foodcat}"><h3>${response[i].foodcat}</h3></div>`
+            }
+            document.querySelector("div.fooditem .container").innerHTML = content;
+
+            //for the options in add food
+            let content0 = "";
+            for (var i = 0; i < response.length; i++) {
+                
+                content0 = `${content0}<option data-foodcat='${response[i]._id}'>${response[i].foodcat}</option>`
+            }
+            document.querySelector("select#foodcatselection").innerHTML = content0;
+            
+        });
+        
+        getMenu();
+    }
+
+    function getMenu(){
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://onlinefood-ef2c.restdb.io/rest/menu",
+            "method": "GET",
+            "headers": {
+              "content-type": "application/json",
+              "x-apikey": APIKEY,
+              "cache-control": "no-cache"
+            }
+        }
+          
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            
+            
+
+            for (var i = 0; i < response.length; i++) {
+                let content = "";
+                content = `
+                <div class="col-md-4">
+                    <div class="card mb-4 box-shadow">
+                        <img class="card-img-top" src="${response[i].foodimageurl}" alt="${response[i].foodname}">
+                        <div class="card-body">
+                            <p class="card-text">${response[i].fooddescription}</p>
+                            <div class="d-flex justify-content-between align-items-center">
+                                <div class="btn-group">
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                                </div>
+                                <small class="text-muted">$${response[i].foodprice.toFixed(2)}</small>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+                catarea(response[i].foodcat[0].foodcat, content);  
+            }
+            removeemptycategory()
+        }); 
+    }
+
+    function catarea(foodcat, content){
+        document.querySelector(`main div.fooditem .container div#${foodcat}`).innerHTML += content;
+    }
+
+    function removeemptycategory(){
+        let rowlist = document.querySelectorAll("div.row");
+        for (i = 0; i < rowlist.length; i++){
+            
+            if (rowlist[i].querySelector('.col-md-4') == null) {
+                rowlist[i].remove();
+            } 
+    
+        }
+        navcategory();
+
+    }
+
+    function navcategory(){
+        let rowlist = document.querySelectorAll("div.row");
+        for (i = 0; i < rowlist.length; i++){
+            let title = rowlist[i].querySelector('h3').textContent;
+            content = `<li><a class="dropdown-item" href="#${rowlist[i].id}">${title}</a></li>`;
+            document.querySelector(".dropdown-menu").innerHTML += content;
+        } 
+    }
+
+
+
+
+
+
+
+})
+
---
+++ assets/mainjs/menu.js
@@ -0,0 +1,149 @@
+$(document).ready(function () {
+    const APIKEY = '61e0110da0f7d226f9b75dbc';
+    getCategory();
+    
+    /*imagesLoaded active*/
+    function filtercat() 
+    {
+        $('#portfolio-grid,.blog-masonry').imagesLoaded(function() {
+
+            /* Filter menu */
+            $('.mix-item-menu').on('click', 'button', function() {
+                var filterValue = $(this).attr('data-filter');
+                $grid.isotope({
+                    filter: filterValue
+                });
+            });
+
+            /* filter menu active class  */
+            $('.mix-item-menu button').on('click', function(event) {
+                $(this).siblings('.active').removeClass('active');
+                $(this).addClass('active');
+                event.preventDefault();
+            });
+
+            /* Filter active */
+            var $grid = $('#portfolio-grid').isotope({
+                itemSelector: '.pf-item',
+                percentPosition: true,
+                masonry: {
+                    columnWidth: '.pf-item',
+                }
+            });
+
+        });
+
+    }
+
+    //create foodcat div.row
+    function getCategory(){
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://onlinefood-ef2c.restdb.io/rest/foodcategory",
+            "method": "GET",
+            "headers": {
+              "content-type": "application/json",
+              "x-apikey": APIKEY,
+              "cache-control": "no-cache"
+            }
+        }
+        
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            let content ="";
+            for (var i = 0; i < response.length; i++) {
+                
+                content = `${content}<button data-filter=".${response[i]._id}">${response[i].foodcat}</button>`
+            }
+            document.querySelector(".food-menu-content .mix-item-menu").innerHTML += content;
+        });
+        
+        getMenu();
+    }
+
+
+    function getMenu(){
+        var settings = {
+            "async": true,
+            "crossDomain": true,
+            "url": "https://onlinefood-ef2c.restdb.io/rest/menu",
+            "method": "GET",
+            "headers": {
+              "content-type": "application/json",
+              "x-apikey": APIKEY,
+              "cache-control": "no-cache"
+            }
+        }
+          
+        $.ajax(settings).done(function (response) {
+            console.log(response);
+            
+            
+
+            for (var i = 0; i < response.length; i++) {
+                let content = "";
+                content = `
+                <div class="item-single pf-item ${response[i].foodcat[0]._id}">
+                    <div class="item">
+                        <div class="thumb">
+                            <a href="#">
+                                <img src="${response[i].foodimageurl}" alt="Thumb" height="300" width="400">
+                            </a>
+                            <div class="price">
+                                <h5>$${response[i].foodprice.toFixed(2)}</h5>
+                            </div>
+                        </div>
+                        <div class="info">
+                            <h4><a href="#">${response[i].foodname}</a></h4>
+                            <p>
+                                ${response[i].fooddescription}
+                            </p>
+                            <div class="button" data-_id='${response[i]._id}'>
+                                <a href="#">ADD TO CART</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+                document.querySelector("div#portfolio-grid.menu-lists").innerHTML += content;
+            }
+            filtercat()
+        }); 
+    function removecaregory(){
+        document.querySelectorAll()
+        array.forEach(element => {
+            
+        });
+    }
+
+
+
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
+
