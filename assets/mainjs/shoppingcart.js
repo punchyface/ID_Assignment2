36ec743f28d408be5ca4717027468196e5e2c7a9
punchyface <s10222101@connect.np.edu.sg>
2022-02-16 02:01:54 +0800

    minor fix

--- assets/mainjs/shoppingcart.js
+++ assets/mainjs/shoppingcart.js
@@ -125,11 +125,11 @@ $(document).ready(function () {
 
             oktaSignIn.session.get(function (res) {
                 user = res.userId
-                product = JSON.stringify(localStorage.getItem('Product Details'))
+                product = JSON.parse(localStorage.getItem('Product Details'))
                 //post to order entity
                 var jsondata = {
                         "user": user,
-                        "product": [{product}],
+                        "product": product,
                         "address": address
                     };
                 var settings = {
