80481b32d1020113c36e8d06be45ea4493294b51
Josephwhx <JosephWonghx@gmail.com>
2022-02-17 11:24:26 +0800

    change layout of form in shoppingcart

--- assets/mainjs/shoppingcart.js
+++ assets/mainjs/shoppingcart.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
         //Go through local storage and update page
         Object.values(itemsInLocalStorage).map(item => {
             //$(".default-cart-preloader").fadeOut(60); //Remove default preloader
-            $(".cart-footer").css('visibility', 'visible'); //Make price visible
+            $(".total-price").css('visibility', 'visible'); //Make price visible
             content += `<tr>
             <!-- Product -->
             <td>
