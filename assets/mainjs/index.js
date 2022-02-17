e78d3554bbac716219c5f530f1ba6e334a6e971e
punchyface <s10222101@connect.np.edu.sg>
2022-02-17 13:56:33 +0800

    testing 1

--- assets/mainjs/index.js
+++ assets/mainjs/index.js
@@ -5,4 +5,22 @@ Preloader Init
 $(window).on('load', function() {
     // Animate loader off screen
     $(".se-pre-con").fadeOut("slow");
-});
+    var oktaSignIn = new OktaSignIn({
+        baseUrl: "https://dev-77878233.okta.com",
+        logo: 'assets/img/logo.png',
+        logoText: 'Shiok Ah',
+        clientId: "0oa3tpe06geyv3Tq25d7",
+        authParams: {
+        issuer: "default",
+        responseType: ['token', 'id_token'],
+        display: 'page'
+        }
+    });
+    oktaSignIn.session.get(function (res){
+        fetch('https://dev-77878233.okta.com/api/v1/users/me')
+            .then(response => response.json()) 
+            .then(function(data){
+                console.log(data)
+            })
+    })
+})
