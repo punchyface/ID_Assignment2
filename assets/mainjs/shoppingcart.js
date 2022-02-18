89333b6ee313dbd89fac145575fd35cf098b5106
punchyface <s10222101@connect.np.edu.sg>
2022-02-18 14:44:25 +0800

    fix post data error

--- assets/mainjs/shoppingcart.js
+++ assets/mainjs/shoppingcart.js
@@ -180,46 +180,45 @@ $(document).ready(function () {
                 //{datetime book in 24h format (DD/MM/YYYY HH:mm:ss)}
                 let arrangedate = $("#arrangedate.form-control").val();
                 let arrangetime = $("#arrangetime.form-control").val();
-                let arrangedatetime = new Date(arrangedate + ' ' + arrangetime);
-                console.log(address);
-                console.log(voucher);
-                console.log(arrangedatetime);
+                let arrangedatetime = arrangedate + ' ' + arrangetime;
                 tocheck(address, order-error);
-
-                //change voucher status
-                var jsondata = {"status": false};
-                var settings = {
-                "async": true,
-                "crossDomain": true,
-                "url": `https://onlinefood-ef2c.restdb.io/rest/voucher/${voucher._id}`,
-                "method": "PATCH",
-                "headers": {
-                    "content-type": "application/json",
-                    "x-apikey": APIKEY,
-                    "cache-control": "no-cache"
-                },
-                "processData": false,
-                "data": JSON.stringify(jsondata),
-                "beforeSend": function(){
-                    $(".check-out-btn button").prop( "disabled", true);
-                    $(".default-cart-preloader").show();
-                    //make body tag unscrollable
-                }
-                }
-
-                $.ajax(settings).done(function (response) {
-                console.log(response);
-                    
-                    //post to order entity
-                    var jsondata = {
-                            "user": user,
-                            "product": product,
-                            "address": address,
-                            "arrangedatetime": arrangedatetime,
-                            "voucher" : [voucher._id],
-                            "remarks" : remarks
-                        };
+                if (voucher == null){
+                     //change voucher status
+                    var jsondata = {"status": false};
                     var settings = {
+                        "async": true,
+                        "crossDomain": true,
+                        "url": `https://onlinefood-ef2c.restdb.io/rest/voucher/${voucher._id}`,
+                        "method": "PATCH",
+                        "headers": {
+                            "content-type": "application/json",
+                            "x-apikey": APIKEY,
+                            "cache-control": "no-cache"
+                        },
+                        "processData": false,
+                        "data": JSON.stringify(jsondata),
+                            "beforeSend": function(){
+                                $(".check-out-btn button").prop( "disabled", true);
+                                $(".default-cart-preloader").show();
+                                //make body tag unscrollable
+                            }
+                    }
+                    $.ajax(settings).done(function (response) {
+                        console.log(response);
+
+                })
+               
+                    
+                //post to order entity
+                var jsondata = {
+                        "user": user,
+                        "product": product,
+                        "address": address,
+                        "arrangedatetime": arrangedatetime,
+                        "voucher" : [voucher._id],
+                        "remarks" : remarks
+                    };
+                var settings = {
                     "async": true,
                     "crossDomain": true,
                     "url": "https://onlinefood-ef2c.restdb.io/rest/order",
@@ -230,52 +229,54 @@ $(document).ready(function () {
                         "cache-control": "no-cache"
                     },
                     "processData": false,
-                    "data": JSON.stringify(jsondata)
+                    "data": JSON.stringify(jsondata),
+                    "beforeSend": function(){
+                        $(".check-out-btn button").prop( "disabled", true);
+                        $(".default-cart-preloader").show();
                     }
-                    $.ajax(settings).done(function (response) {
-                        console.log(response);
-                        let totalsum = 0.00;
-                        var allsubtotals = document.getElementsByClassName('subtotal')
-                        for(var i = 0; i < allsubtotals.length; i++){
-                            var subtotal = allsubtotals[i];
-                            totalsum += parseFloat(subtotal.innerHTML);
-                        }
+                }
+                $.ajax(settings).done(function (response) {
+                    console.log(response);
+                    let totalsum = 0.00;
+                    var allsubtotals = document.getElementsByClassName('subtotal')
+                    for(var i = 0; i < allsubtotals.length; i++){
+                        var subtotal = allsubtotals[i];
+                        totalsum += parseFloat(subtotal.innerHTML);
+                    }
 
-                        let minspend = 50
-                        if (totalsum >= minspend){
-                            var jsondata = {"user": user};
-                            var settings = {
-                                "async": true,
-                                "crossDomain": true,
-                                "url": "https://onlinefood-ef2c.restdb.io/rest/game",
-                                "method": "POST",
-                                "headers": {
-                                    "content-type": "application/json",
-                                    "x-apikey": APIKEY,
-                                    "cache-control": "no-cache"
-                                },
-                                "processData": false,
-                                "data": JSON.stringify(jsondata)
-                            }
-                            $.ajax(settings).done(function (response) {
-                                console.log(response);
-                                document.querySelector("#modal-checkout .modal-para").innerHTML += `<p><a class="btn btn-primary" href="#" id="game">You earn a chance to spin the wheel</a></p>`
-                                //Clear local storage
-                                localStorage.clear();
-                                //show popup/modal
-                                $(document).ready(showModal());
-                            });
+                    let minspend = 50
+                    if (totalsum >= minspend){
+                        var jsondata = {"user": user};
+                        var settings = {
+                            "async": true,
+                            "crossDomain": true,
+                            "url": "https://onlinefood-ef2c.restdb.io/rest/game",
+                            "method": "POST",
+                            "headers": {
+                                "content-type": "application/json",
+                                "x-apikey": APIKEY,
+                                "cache-control": "no-cache"
+                            },
+                            "processData": false,
+                            "data": JSON.stringify(jsondata)
                         }
-                        else{
+                        $.ajax(settings).done(function (response) {
+                            console.log(response);
+                            document.querySelector("#modal-checkout .modal-para").innerHTML += `<p><a class="btn btn-primary" href="#" id="game">You earn a chance to spin the wheel</a></p>`
                             //Clear local storage
                             localStorage.clear();
                             //show popup/modal
-                            showModal();
-                        }
-                    })
-                })
-                    
-            })
+                            $(document).ready(showModal());
+                        });
+                    }
+                    else{
+                        //Clear local storage
+                        localStorage.clear();
+                        //show popup/modal
+                        showModal();
+                    }
+                })  
+            }
         })
 
     }
